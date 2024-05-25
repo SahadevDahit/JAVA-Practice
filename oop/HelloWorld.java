99d8c50bce105b7e8a7beeb72f2b29c913e281a1
SahadevDahit <sahadevdahit123@gmail.com>
2024-05-25 17:16:28 +0545

    more

--- oop/HelloWorld.java
+++ oop/HelloWorld.java
@@ -7,30 +7,11 @@ import java.util.LinkedList;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		// push 0 to the end
-		int[] array= {1,2,3,0,0,6,0,8,0,0};
-		
-		// case 1 left and right non zero
-		// case 2 left 0 and right non zero 
-		// case 3 left and right both zero, 
-		int left=0, right=1;
-		for(int i=right; i<array.length; i++) {
-			if(array[left]!=0) {
-				left++;
-				right++;
-			}else if(array[right]==0){
-				right++;
-			}else {
-				int temp=array[right];
-				array[right]=array[left];
-				array[left]=temp;
-			}
-		}
-		
-		for(int i: array){
-			System.out.println(i);
-		}
+		// TODO Auto-generated method stub
+		// Matrix rotation
 		
 		
+		
+	
 	}
 }
